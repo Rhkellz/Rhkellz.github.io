9065bf44678941e319270b6b5038a8208945a9ee
Rhkellz <67848704+Rhkellz@users.noreply.github.com>
2023-10-15 17:46:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let date = new Date().setHours(12, 0, 0, 0)
+let date// = new Date().setHours(12, 0, 0, 0)
 let day
 function time() {
-   date = date + 10
+   date = new Date()
    day = 2//date.getDay()
 }
 
