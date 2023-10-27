43d80fa404d66ecc755fc3749c4e541c6e3162de
Rhkellz <67848704+Rhkellz@users.noreply.github.com>
2023-10-27 10:15:57 -0500

    Update stats.js

--- stats/stats.js
+++ stats/stats.js
@@ -178,9 +178,9 @@ function updateTimer() {
                                 weekCompleted += getPeriods(data[k].typeday)[getPeriods(data[k].typeday).length-1].end - getPeriods(data[k].typeday)[0].start
                             }
                             if (date > getPeriods(data[currentDay].typeday)[getPeriods(data[currentDay].typeday).length-1].end) {
-                                weekCompleted += getPeriods(data[currentDay].typeday)[getPeriods(data[currentDay].typeday).length-1].end - new Date()
-                            } else {
                                 //weekCompleted += getPeriods(data[currentDay].typeday)[getPeriods(data[currentDay].typeday).length-1].end - getPeriods(data[currentDay].typeday)[0].start
+                            } else {
+                                weekCompleted += getPeriods(data[currentDay].typeday)[getPeriods(data[currentDay].typeday).length-1].end - new Date()
                             }
                             //console.log(weekCompleted)
                             let weekPercent = Math.floor((weekCompleted / weekTotal) * 100)
