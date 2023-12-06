519236280531bc26e243bd020dd6e4fff8425332
Rhkellz <67848704+Rhkellz@users.noreply.github.com>
2023-12-05 19:53:13 -0600

    Update main.js
    progress bars, period names, rudimentary stats menu

--- main.js
+++ main.js
@@ -7,16 +7,17 @@ function time() {
 }
 
 function twoDigits(num) {
-  return num.toString().padStart(2, '0')
+  return num.toString().padStart(2, "0")
 }
 
 function timer(x) {
+  date = new Date().setHours(13, 10, 0, 0)
   for (let i = 0; i < x.length; i++) {
     if (date < x[i].start) {
-      return [x[i].start - date, x[i].start - x[i-1].end]
+      return [x[i].start - date, x[i].start - x[i-1].end, x[i].name]
       }
     if (date < x[i].end) {
-      return [x[i].end - date, x[i].end - x[i].start]
+      return [x[i].end - date, x[i].end - x[i].start, "Passing Period"]
     }
   }
   return -1
@@ -26,38 +27,38 @@ function getPeriods(x) {
     switch (x) {
       case 1:
         return [
-          {start: new Date().setHours(8, 30, 0, 0), end: new Date().setHours(9, 3, 0, 0)},
-          {start: new Date().setHours(9, 8, 0, 0), end: new Date().setHours(9, 41, 0, 0)},
-          {start: new Date().setHours(9, 46, 0, 0), end: new Date().setHours(10, 19, 0, 0)},
-          {start: new Date().setHours(10, 24, 0, 0), end: new Date().setHours(10, 57, 0, 0)},
-          {start: new Date().setHours(11, 2, 0, 0), end: new Date().setHours(11, 35, 0, 0)},
-          {start: new Date().setHours(11, 40, 0, 0), end: new Date().setHours(12, 13, 0, 0)},
-          {start: new Date().setHours(12, 18, 0, 0), end: new Date().setHours(12, 51, 0, 0)},
-          {start: new Date().setHours(12, 56, 0, 0), end: new Date().setHours(13, 29, 0, 0)},
-          {start: new Date().setHours(13, 34, 0, 0), end: new Date().setHours(14, 7, 0, 0)}, 
-          {start: new Date().setHours(14, 7, 0, 0), end: new Date().setHours(14, 7, 0, 0)}
+          {start: new Date().setHours(8, 30, 0, 0), end: new Date().setHours(9, 3, 0, 0), name: "Block 1"},
+          {start: new Date().setHours(9, 8, 0, 0), end: new Date().setHours(9, 41, 0, 0), name: "Block 2"},
+          {start: new Date().setHours(9, 46, 0, 0), end: new Date().setHours(10, 19, 0, 0), name: "Block 3"},
+          {start: new Date().setHours(10, 24, 0, 0), end: new Date().setHours(10, 57, 0, 0), name: "Block 4"},
+          {start: new Date().setHours(11, 2, 0, 0), end: new Date().setHours(11, 35, 0, 0), name: "Block 5A"},
+          {start: new Date().setHours(11, 40, 0, 0), end: new Date().setHours(12, 13, 0, 0), name: "Block 5B"},
+          {start: new Date().setHours(12, 18, 0, 0), end: new Date().setHours(12, 51, 0, 0), name: "Block 6"},
+          {start: new Date().setHours(12, 56, 0, 0), end: new Date().setHours(13, 29, 0, 0), name: "Block 7"},
+          {start: new Date().setHours(13, 34, 0, 0), end: new Date().setHours(14, 7, 0, 0), name: "Block 8"}, 
+          {start: new Date().setHours(14, 7, 0, 0), end: new Date().setHours(14, 7, 0, 0), name: "Block 8"}
         ]
       case 2:
         return [
-          {start: new Date().setHours(8, 30, 0, 0), end: new Date().setHours(9, 55, 0, 0)},
-          {start: new Date().setHours(10, 5, 0, 0), end: new Date().setHours(11, 30, 0, 0)},
-          {start: new Date().setHours(11, 40, 0, 0), end: new Date().setHours(13, 5, 0, 0)},
-          {start: new Date().setHours(13, 15, 0, 0), end: new Date().setHours(13, 55, 0, 0)},
-          {start: new Date().setHours(14, 5, 0, 0), end: new Date().setHours(15, 35, 0, 0)},
+          {start: new Date().setHours(8, 30, 0, 0), end: new Date().setHours(9, 55, 0, 0), name: "Block 1"},
+          {start: new Date().setHours(10, 5, 0, 0), end: new Date().setHours(11, 30, 0, 0), name: "Block 3"},
+          {start: new Date().setHours(11, 40, 0, 0), end: new Date().setHours(13, 5, 0, 0), name: "Block 5A"},
+          {start: new Date().setHours(13, 15, 0, 0), end: new Date().setHours(13, 55, 0, 0), name: "Lunch 2"},
+          {start: new Date().setHours(14, 5, 0, 0), end: new Date().setHours(15, 35, 0, 0), name: "Block 7"},
           //{start: new Date().setHours(15, 35, 0, 0), end: new Date().setHours(15, 35, 0, 0)}//fixes a 2:07 bug because i dont know how my code works 
         ]
         
       case 3:
         return [
-          {start: new Date().setHours(8, 30, 0, 0), end: new Date().setHours(9, 55, 0, 0)},
-          {start: new Date().setHours(10, 5, 0, 0), end: new Date().setHours(11, 30, 0, 0)},
-          {start: new Date().setHours(11, 40, 0, 0), end: new Date().setHours(13, 5, 0, 0)},
-          {start: new Date().setHours(13, 15, 0, 0), end: new Date().setHours(13, 55, 0, 0)},
-          {start: new Date().setHours(14, 5, 0, 0), end: new Date().setHours(15, 35, 0, 0)},
+          {start: new Date().setHours(8, 30, 0, 0), end: new Date().setHours(9, 55, 0, 0), name: "Block 2"},
+          {start: new Date().setHours(10, 5, 0, 0), end: new Date().setHours(11, 30, 0, 0), name: "Block 4"},
+          {start: new Date().setHours(11, 40, 0, 0), end: new Date().setHours(13, 5, 0, 0), name: "Block 6A"},
+          {start: new Date().setHours(13, 15, 0, 0), end: new Date().setHours(13, 55, 0, 0), name: "Lunch 2"},
+          {start: new Date().setHours(14, 5, 0, 0), end: new Date().setHours(15, 35, 0, 0), name: "Block 8"},
           //{start: new Date().setHours(15, 35, 0, 0), end: new Date().setHours(15, 35, 0, 0)}//fixes a 2:07 bug because i dont know how my code works 
         ]
       case 5:
-        return [{start: new Date().setHours(8, 0, 0, 0), end: new Date().setHours(12, 0, 0, 0)}]
+        return [{start: new Date().setHours(8, 0, 0, 0), end: new Date().setHours(12, 0, 0, 0), name: "Full Period"}]
       default:
         break
     }
@@ -76,8 +77,11 @@ function updateTimer() {
   let typeDay
   let sStart = 510
   let sEnd
+  let name
+  let altPeriodsName
+  let periodsName
   let obar = new Date().setHours(11, 40, 0, 0)
-  fetch('./data/monthschedule.json')
+  fetch("./data/monthschedule.json")
       .then(function(resp) {
         return resp.json()
       })
@@ -88,9 +92,22 @@ function updateTimer() {
               typeDay = data[i].typeday
               switch (typeDay) {
                 case 1://wildkit monday
+                  obar = new Date().setHours(11, 2, 0, 0)
+                  operiods = [
+                    {start: new Date().setHours(11, 2, 0, 0), end: new Date().setHours(11, 35, 0, 0), name: "Lunch 1"},
+                    {start: new Date().setHours(11, 40, 0, 0), end: new Date().setHours(12, 13, 0, 0), name: "Lunch 2"}
+                  ]
+                  if (date < obar) {
+                    otimeleft = -1
+                  } else {
+                    otimeleft = timer(operiods)[0]
+                    opercent = Math.floor(100 - (timer(operiods)[0] / timer(operiods)[1]) * 100)
+                    altPeriodsName = timer(operiods)[2]
+                  }
                   periods = getPeriods(typeDay)
                   document.body.style.backgroundColor = "black"
                   timeleft = timer(periods)[0]
+                  periodsName = timer(periods)[2]
                   otimeleft = -1
                   sEnd = 847
                   percent = Math.floor(100 - (timer(periods)[0] / timer(periods)[1]) * 100)
@@ -98,17 +115,19 @@ function updateTimer() {
                 case 2://orange day
                   periods = getPeriods(typeDay)
                   operiods = [
-                    {start: new Date().setHours(11, 40, 0, 0), end: new Date().setHours(12, 20, 0, 0)},
-                    {start: new Date().setHours(12, 20, 0, 0), end: new Date().setHours(12, 30, 0, 0)},//fix for bug i dont understand, may cause issues
-                    {start: new Date().setHours(12, 30, 0, 0), end: new Date().setHours(13, 55, 0, 0)}
+                    {start: new Date().setHours(11, 40, 0, 0), end: new Date().setHours(12, 20, 0, 0), name: "Lunch 1"},
+                    {start: new Date().setHours(12, 20, 0, 0), end: new Date().setHours(12, 30, 0, 0), name: "Passing Period"},//fix for bug i dont understand, may cause issues
+                    {start: new Date().setHours(12, 30, 0, 0), end: new Date().setHours(13, 55, 0, 0), name: "Block 5B"}
                   ]
                   document.body.style.backgroundColor = "#ff7000"
                   timeleft = timer(periods)[0]
+                  periodsName = timer(periods)[2]
                   if (date < obar) {
                     otimeleft = -1
                   } else {
                     otimeleft = timer(operiods)[0]
                     opercent = Math.floor(100 - (timer(operiods)[0] / timer(operiods)[1]) * 100)
+                    altPeriodsName = timer(operiods)[2]
                   }
                   sEnd = 935
                   percent = Math.floor(100 - (timer(periods)[0] / timer(periods)[1]) * 100)
@@ -116,17 +135,19 @@ function updateTimer() {
                 case 3://blue days
                   periods = getPeriods(typeDay)
                   operiods = [
-                    {start: new Date().setHours(11, 40, 0, 0), end: new Date().setHours(12, 20, 0, 0)},
-                    {start: new Date().setHours(12, 20, 0, 0), end: new Date().setHours(12, 30, 0, 0)},
-                    {start: new Date().setHours(12, 30, 0, 0), end: new Date().setHours(13, 55, 0, 0)}
+                    {start: new Date().setHours(11, 40, 0, 0), end: new Date().setHours(12, 20, 0, 0), name: "Lunch 1"},
+                    {start: new Date().setHours(12, 20, 0, 0), end: new Date().setHours(12, 30, 0, 0), name: "Passing Period"},
+                    {start: new Date().setHours(12, 30, 0, 0), end: new Date().setHours(13, 55, 0, 0), name: "Block 6B"}
                   ]
                   document.body.style.backgroundColor = "#125e70"
                   timeleft = timer(periods)[0]
+                  periodsName = timer(periods)[2]
                   if (date < obar) {
                     otimeleft = -1
                   } else {
                     otimeleft = timer(operiods)[0]
                     opercent = Math.floor(100 - (timer(operiods)[0] / timer(operiods)[1]) * 100)
+                    altPeriodsName = timer(operiods)[2]
                   }
                   sEnd = 935
                   percent = Math.floor(100 - (timer(periods)[0] / timer(periods)[1]) * 100)
@@ -141,6 +162,7 @@ function updateTimer() {
                   otimeleft = -1
                   sEnd = 720
                   percent = Math.floor(100 - (timer(periods)[0] / timer(periods)[1]) * 100)
+                  periodsName = timer(periods)[2]
                   break
                 default:
                   timeleft = -1
@@ -169,23 +191,41 @@ function updateTimer() {
               const oseconds = Math.floor(otimeleft / 1000) % 60
 
               if (otimeleft == -1 || date < obar || otimeleft == null) {
-                document.getElementById("clock").innerText =`${twoDigits(minutes)}:${twoDigits(seconds)}` + " left " + percent + "%"
+                document.getElementById("clock").innerText = periodsName + ": " + `${twoDigits(minutes)}:${twoDigits(seconds)}` + " left"
+                document.getElementById("progress1").style.width = percent + "%"
+                document.getElementById("progress2").style.display = "none"
+                document.getElementById("classPercent").innerText = periodsName + " is " + percent + "% completed"
               } else {
-                document.getElementById("clock").innerText =`${twoDigits(minutes)}:${twoDigits(seconds)}` + " left " + percent + "%"; document.getElementById("percent").innerText = `${twoDigits(ominutes)}:${twoDigits(oseconds)}` + " left " + opercent + "%"
+                document.getElementById("clock").innerText = periodsName + ": " + `${twoDigits(minutes)}:${twoDigits(seconds)}` + " left"
+                document.getElementById("progress1").style.width = percent + "%"
+                document.getElementById("percent").innerText = altPeriodsName + ": " + `${twoDigits(ominutes)}:${twoDigits(oseconds)}` + " left"
+                document.getElementById("progress2").style.width = opercent + "%"
+                document.getElementById("classPercent").innerText = periodsName + " is " + percent + "% completed"
+                document.getElementById("altClassPercent").innerText = altPeriodsName + " is " + opercent + "% completed"
               }
               
-            
+              /*
               if (dateTime >= sEnd || timeleft == -1) { 
                 document.getElementById("clock").innerText = "School's over"
+
+                document.getElementById("progress1").style.display = "none"
+                document.getElementById("progress2").style.display = "none"
               } else if (dateTime < sStart) {
                 document.getElementById("clock").innerText = "School hasn't started"
+
+                document.getElementById("progress1").style.display = "none"
+                document.getElementById("progress2").style.display = "none"
               }
               if (typeDay == 4) {
                 document.getElementById("clock").innerText = "No School"
+
+                document.getElementById("progress1").style.display = "none"
+                document.getElementById("progress2").style.display = "none"
               }
               if (document.body.style.backgroundColor == "black") {
-                document.getElementById("githubLink").color = "white"
-              }
+                document.getElementById("githubLink").style.color = "white"
+                document.getElementById("progress1").style.color = "white"
+              }*/
             }
           }
         }
@@ -221,7 +261,7 @@ function updateTimer() {
                      weekCompleted += new Date() - getPeriods(data[dayAsIndex].typeday)[0].start
                      weekPercent = Math.floor((weekCompleted / weekTotal) * 100)
                  }
-                 document.getElementById("weekPercent").innerText = weekPercent + "% school week completed"
+                 document.getElementById("weekPercent").innerText = "The school week is " + weekPercent + "% completed"
                  break
               }
          }
@@ -230,23 +270,14 @@ function updateTimer() {
       })
 }
 
-function toggleStats() {
-  var x = document.getElementById("weekPercent");
-  var y = document.getElementById("dayPercent");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    y.style.display = "none";
-  } else {
-    x.style.display = "block";
-    y.style.display = "block";
-  }
-}
-
-document.getElementById("toggleStats").onclick = toggleStats
 
 function updateUpdateTimer() {
    setInterval(time, 10)
    setInterval(updateTimer, 10)
 }
 
-window.addEventListener('load', updateUpdateTimer)
+  document.querySelector(".toggle").addEventListener("click", function(){
+  document.querySelector("aside").classList.toggle("visible")})
+
+
+window.addEventListener("load", updateUpdateTimer)
