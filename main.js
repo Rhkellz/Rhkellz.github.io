5b21cb5c23eb70a8f5c618bfa53bfa42fa9a2456
Rhkellz <67848704+Rhkellz@users.noreply.github.com>
2024-02-27 10:23:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -62,9 +62,9 @@ function getPeriods(x) {
           //{start: new Date().setHours(15, 35, 0, 0), end: new Date().setHours(15, 35, 0, 0)}//fixes a 2:07 bug because i dont know how my code works 
         ]
       case 4:
-          return [
-             {start: new Date().setHours(8, 30, 0, 0), end: new Date().setHours(8, 30, 0, 0), name: "Block 2"},
-             {start: new Date().setHours(8:, 30, 0, 0), end: new Date().setHours(8, 30, 0, 0), name: "Block 4"}]
+        return [
+           {start: new Date().setHours(8, 30, 0, 0), end: new Date().setHours(8, 30, 0, 0), name: "Block 2"},
+           {start: new Date().setHours(8, 30, 0, 0), end: new Date().setHours(8, 30, 0, 0), name: "Block 4"}]
       case 5:
         return [{start: new Date().setHours(8, 0, 0, 0), end: new Date().setHours(12, 0, 0, 0), name: "Full Period"}]
       case 6:
